a5b06c7a8c8966bc441824d22e07495df0258a17
webdev-web2go <desarrollos.web2go@gmail.com>
2023-12-20 17:54:51 -0600

    Add 'event' table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,10 @@
 import { relations, sql } from "drizzle-orm";
 import {
   bigint,
+  boolean,
+  datetime,
   index,
-  int,
   mysqlTableCreator,
-  primaryKey,
-  text,
   timestamp,
   varchar,
 } from "drizzle-orm/mysql-core";
@@ -19,92 +18,91 @@ import { type AdapterAccount } from "next-auth/adapters";
  */
 export const mysqlTable = mysqlTableCreator((name) => `izzymo-sound_${name}`);
 
-export const posts = mysqlTable(
-  "post",
+export const events = mysqlTable(
+  "event",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("createdById", { length: 255 }).notNull(),
+    title: varchar("title", { length: 256 }).notNull(),
+    start: datetime("start").notNull(),
+    allDay: boolean("all_day").default(true),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
   },
   (example) => ({
-    createdByIdIdx: index("createdById_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  })
+    nameIndex: index("name_idx").on(example.title),
+  }),
 );
 
-export const users = mysqlTable("user", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("emailVerified", {
-    mode: "date",
-    fsp: 3,
-  }).default(sql`CURRENT_TIMESTAMP(3)`),
-  image: varchar("image", { length: 255 }),
-});
+// export const users = mysqlTable("user", {
+//   id: varchar("id", { length: 255 }).notNull().primaryKey(),
+//   name: varchar("name", { length: 255 }),
+//   email: varchar("email", { length: 255 }).notNull(),
+//   emailVerified: timestamp("emailVerified", {
+//     mode: "date",
+//     fsp: 3,
+//   }).default(sql`CURRENT_TIMESTAMP(3)`),
+//   image: varchar("image", { length: 255 }),
+// });
 
-export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
-  sessions: many(sessions),
-}));
+// export const usersRelations = relations(users, ({ many }) => ({
+//   accounts: many(accounts),
+//   sessions: many(sessions),
+// }));
 
-export const accounts = mysqlTable(
-  "account",
-  {
-    userId: varchar("userId", { length: 255 }).notNull(),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: int("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey(account.provider, account.providerAccountId),
-    userIdIdx: index("userId_idx").on(account.userId),
-  })
-);
+// export const accounts = mysqlTable(
+//   "account",
+//   {
+//     userId: varchar("userId", { length: 255 }).notNull(),
+//     type: varchar("type", { length: 255 })
+//       .$type<AdapterAccount["type"]>()
+//       .notNull(),
+//     provider: varchar("provider", { length: 255 }).notNull(),
+//     providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+//     refresh_token: text("refresh_token"),
+//     access_token: text("access_token"),
+//     expires_at: int("expires_at"),
+//     token_type: varchar("token_type", { length: 255 }),
+//     scope: varchar("scope", { length: 255 }),
+//     id_token: text("id_token"),
+//     session_state: varchar("session_state", { length: 255 }),
+//   },
+//   (account) => ({
+//     compoundKey: primaryKey(account.provider, account.providerAccountId),
+//     userIdIdx: index("userId_idx").on(account.userId),
+//   }),
+// );
 
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
+// export const accountsRelations = relations(accounts, ({ one }) => ({
+//   user: one(users, { fields: [accounts.userId], references: [users.id] }),
+// }));
 
-export const sessions = mysqlTable(
-  "session",
-  {
-    sessionToken: varchar("sessionToken", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("userId", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("userId_idx").on(session.userId),
-  })
-);
+// export const sessions = mysqlTable(
+//   "session",
+//   {
+//     sessionToken: varchar("sessionToken", { length: 255 })
+//       .notNull()
+//       .primaryKey(),
+//     userId: varchar("userId", { length: 255 }).notNull(),
+//     expires: timestamp("expires", { mode: "date" }).notNull(),
+//   },
+//   (session) => ({
+//     userIdIdx: index("userId_idx").on(session.userId),
+//   }),
+// );
 
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
+// export const sessionsRelations = relations(sessions, ({ one }) => ({
+//   user: one(users, { fields: [sessions.userId], references: [users.id] }),
+// }));
 
-export const verificationTokens = mysqlTable(
-  "verificationToken",
-  {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (vt) => ({
-    compoundKey: primaryKey(vt.identifier, vt.token),
-  })
-);
+// export const verificationTokens = mysqlTable(
+//   "verificationToken",
+//   {
+//     identifier: varchar("identifier", { length: 255 }).notNull(),
+//     token: varchar("token", { length: 255 }).notNull(),
+//     expires: timestamp("expires", { mode: "date" }).notNull(),
+//   },
+//   (vt) => ({
+//     compoundKey: primaryKey(vt.identifier, vt.token),
+//   }),
+// );
