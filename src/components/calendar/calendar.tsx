48e1a1b03261ed756523baafb010f5e28cb0629a
webdev-web2go <desarrollos.web2go@gmail.com>
2024-01-11 12:02:35 -0600

    Add admin UI to update equipment reservations data

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -2,26 +2,65 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+import { updateEventAction } from "~/app/[locale]/admin/event-actions";
+import ChooseEquipmentDialog from "./choose-equipment-dialog";
+import DeleteEquipmentDialog from "./delete-equipment-dialog";
+import { Event } from "~/types";
 
-export default function Calendar() {
+interface Props {
+  events: (Event & { id: string })[];
+}
+
+export default function Calendar({ events }: Props) {
+  const [openReservation, setOpenReservation] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+  const [eventToDelete, setEventToDelete] = useState<Event | null>(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
   return (
-    <div className="aspect-square w-[900px]">
+    <div className="mt-28 aspect-square w-full">
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
-        dateClick={() => console.log("date click")}
-        events={[
-          {
-            title: "Modelo EKX200",
-            start: new Date(),
-            end: new Date("2023-12-26T24:00:00Z"),
-            allDay: true,
-          },
-        ]}
+        dateClick={(e) => {
+          if (e.date.getTime() >= new Date().setHours(0, 0, 0, 0)) {
+            setOpenReservation(true);
+            setStartDate(e.date.toISOString().split("T")[0]!);
+            setEndDate(e.date);
+          }
+        }}
+        events={events}
+        eventResize={async (e) => {
+          if (
+            e.event.end?.setHours(0, 0, 0, 0)! <
+            new Date().setHours(0, 0, 0, 0) + 24 * 60 * 60 * 1000
+          ) {
+            e.revert();
+            return;
+          }
+
+          await updateEventAction(Number(e.event.id), e.event.end!);
+        }}
+        eventClick={(e) => {
+          setEventToDelete(e.event as Event);
+          setOpenDelete(true);
+        }}
         nowIndicator={true}
         editable={true}
         selectable={true}
-        selectMirror={true}
         height="100%"
+      />
+      <ChooseEquipmentDialog
+        endDate={endDate}
+        startDate={startDate}
+        open={openReservation}
+        setOpen={setOpenReservation}
+      />
+      <DeleteEquipmentDialog
+        event={eventToDelete as Event}
+        open={openDelete}
+        setOpen={setOpenDelete}
       />
     </div>
   );
