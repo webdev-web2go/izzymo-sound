b6b177308c3424527d6b7b0eda1c657cb688da7b
webdev-web2go <desarrollos.web2go@gmail.com>
2024-01-12 17:31:15 -0600

    Fix responsive

--- src/components/shared/main-banner/main-banner.tsx
+++ src/components/shared/main-banner/main-banner.tsx
@@ -10,7 +10,7 @@ export default function MainBanner({ translation }: { translation: string }) {
   const messages = useMessages();
   return (
     <div className="flex w-full flex-col-reverse items-center justify-between gap-4 md:flex-row">
-      <div className="z-20 flex flex-col-reverse gap-2 sm:flex-row sm:items-center">
+      <div className="z-20 flex flex-col-reverse gap-2 self-start sm:flex-row sm:items-center">
         <div className="h-1.5 w-48 bg-white" />
         <h1 className="text-balance text-5xl font-bold text-white sm:text-6xl">
           {tHome(translation)}
